d9545df85f75ab309a72b9ac5002da128cfbe4a4
me-foogy <sworupkarki1020@gmail.com>
2025-11-19 22:27:45 +0545

    upload after removing the secret key

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,7 +1,10 @@
 export default function Header(){
     return(
         <>
-        
+         <header className="flex justify-center bg-white p-3 items-center rounded-t-2xl shadow-md">
+            <img src="./src/assets/brain.png" className="inline w-1/6"></img>
+            <p className="pl-1">chef.ai</p>
+         </header>
         </>
     )
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import Header from "./Header/header.jsx"
+import MainSection from "./main/mainSection.jsx"
+import Recipie from "./main/recipieSection.jsx"
+import {useState} from 'react';
+
 
 export default function App(){
+    const [recipieShown, setRecipieShown] = useState(false);
+    //setter function handler
+    function showRecipie(){
+        setRecipieShown((prevRecipie)=> prevRecipie?false:true)
+    }
+
     return(
-        <div className="mainElement">
-        <Header/>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-[#edecec] h-[450px] w-[360px] m-auto rounded-2xl items-center shadow-md">
+                <Header/>
+                {!recipieShown?<MainSection handleRecipieButtonPress={showRecipie}/>:<Recipie/>}
+            </div>
         </div>
     )
 }
